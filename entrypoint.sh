d2b0aaaa6f01cab92876a0a47dd88b400da14983
Neil Blue <neil.the.blue+gh1@gmail.com>
2016-06-11 10:28:12 +0100

    Update entrypoint.sh
    Fix to account for args with spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,20 @@ echo "config dnsmasq conf"
 
 CONF_FILE=/etc/dnsmasq.conf
 ARGS=""
-for C in $@
+
+while [ "$1" ]
 do
-  case $C in
-    CONF+*) echo ${C:5} >> $CONF_FILE
+  case $1 in
+    CONF+*) echo "${1}" | sed -e 's/^CONF+//' >> $CONF_FILE
       ;;
-    CONF-*) sed -i -e "/${C:5}/d" $CONF_FILE
+    CONF-*) C=$(echo "${1}" | sed -e 's/^CONF-//'); sed -i -e "/${C}/d" $CONF_FILE
       ;;
-    *) ARGS="${ARGS} $C"
+    *) ARGS="${ARGS} ${1}"
       ;;
   esac
+  shift
 done
 
 dnsmasq -k ${ARGS}
+
+
